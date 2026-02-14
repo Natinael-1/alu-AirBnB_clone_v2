484cb4b83caec6f5be598e762cd0f61fa18c5a33
owizdom <gokun4621@gmail.com>
2026-02-14 08:14:41 +0200

    hmmm

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,49 @@
 #!/usr/bin/env bash
-# Install nginx and configure web static
+# Sets up the web servers for the deployment of web_static
 
+# Install nginx if not already installed
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
+# Create required directories
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
+# Create fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create / recreate symbolic link
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
+# Set ownership
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/server {/a \
-    location /hbnb_static {\
-        alias /data/web_static/current/;\
-    }\
-' /etc/nginx/sites-available/default
+# Configure nginx
+sudo bash -c 'cat > /etc/nginx/sites-available/default <<EOF
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}
+EOF'
+
+# Restart nginx
 sudo service nginx restart
